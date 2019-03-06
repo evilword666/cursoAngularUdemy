3e411f2787772101b22fb8c004685a6389371e9a
Javier Avelino <evilword.com@gmail.com>
2019-03-06 16:43:09 -0600

    Cambios minimos

--- src/app/externo/externo.component.ts
+++ src/app/externo/externo.component.ts
@@ -12,6 +12,7 @@ export class ExternoComponent implements OnInit {
   public userId:number;
   public fecha:any;
   public new_user:any;
+  public usuario_guardado:any;
 
   constructor(
     private _peticionesService: PeticionesService
@@ -62,7 +63,18 @@ this.cargaUsuario();
 
   }
 
-
+onSubmit(form){
+  this._peticionesService.addUser(this.new_user).subscribe(
+    response=>{
+      this.usuario_guardado = response;
+      console.log(response)
+      form.reset();
+    },
+    error=>{
+      console.log(<any>error);
+    }
+  )
+}
 
 
 }
